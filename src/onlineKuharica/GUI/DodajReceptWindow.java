e0467fbc2ccefb2afa7ed4eda32518fcdc26c138
AdmirMazalovci <mazalovic.admir@gmail.com>
2020-03-08 22:32:05 +0100

    GUI za DodajReceptWindow implemenitran

--- src/onlineKuharica/GUI/DodajReceptWindow.java
+++ src/onlineKuharica/GUI/DodajReceptWindow.java
@@ -23,10 +23,6 @@ public class DodajReceptWindow extends JFrame {
         dodajReceptiFrame.setContentPane(new ImagePanel(backgroundImage));
         dodajReceptiFrame.setSize(1200, 800);
 
-        JButton snimiRecept = new JButton("Snimi recept");
-        snimiRecept.setBounds(910, 160, 140, 30);
-        dodajReceptiFrame.add(snimiRecept);
-
         // Dropdown lista svih kuhinja koje je moguce odabrati za jelo
         Kuhinja kuhinja = new Kuhinja();
         ArrayList<Kuhinja> kuhinje = kuhinja.getAllKuhinja();
@@ -51,7 +47,7 @@ public class DodajReceptWindow extends JFrame {
         }
         JComboBox<String> vrstaJelaList = new JComboBox<>(imenaVrstaJelaArray);
 
-        // Dropdown lsita za tezinu pripreme
+        // Dropdown lista za tezinu pripreme
         String[] tezinaPripreme = {TezinaPripreme.LAKO.toString(), TezinaPripreme.SREDNJE.toString(), TezinaPripreme.TESKO.toString()};
         JComboBox<String> tezinaPripremeList = new JComboBox<>(tezinaPripreme);
 
@@ -77,9 +73,8 @@ public class DodajReceptWindow extends JFrame {
         };
         JComboBox<String> mjerneJediniceList = new JComboBox<>(mjerneJediniceArray);
 
-
-
         // Polja za unos podataka za jelo
+        // pozicija labela i text field-ova
         int yLabelDefaultPosition = 50;
         int xLabelDefaultPosition = 50;
         int labelDefaultWidth = 300;
@@ -165,24 +160,22 @@ public class DodajReceptWindow extends JFrame {
         JLabel opisPripremeLabel = new JLabel();
         opisPripremeLabel.setText("<html><p><b><font size = \"5\" color = \"white\">Opis pripreme</p></html>");
         JTextField opisPripremeTextField = new JTextField();
-        yLabelDefaultPosition += 50;
-        yTextFieldDefaultPosition += 50;
-        opisPripremeLabel.setBounds(xLabelDefaultPosition, yLabelDefaultPosition, labelDefaultWidth, labelDefaultHight);
-        opisPripremeTextField.setBounds(xTextFieldDefaultPosition, yTextFieldDefaultPosition, 700, 300);
+        //yLabelDefaultPosition += 50;
+        //yTextFieldDefaultPosition += 50;
+        opisPripremeLabel.setBounds(480, 150, labelDefaultWidth, labelDefaultHight);
+        opisPripremeTextField.setBounds(480, 200, 520, 180);
         dodajReceptiFrame.add(opisPripremeLabel);
         dodajReceptiFrame.add(opisPripremeTextField);
 
 
         // Dodaj namirnicu sekcija
         JButton dodajSastojakButton = new JButton("Dodaj sastojak");
-        dodajSastojakButton.setBounds(50, 600, 140, 30);
+        yLabelDefaultPosition += 50;
+        dodajSastojakButton.setBounds(xTextFieldDefaultPosition, yLabelDefaultPosition, 140, 30);
         dodajReceptiFrame.add(dodajSastojakButton);
         DefaultTableModel model = new DefaultTableModel();
         JTable dodajNamirnicuTabela = new JTable(model);
-        dodajNamirnicuTabela.setBounds(50, 550, 500, dodajNamirnicuTabelaHeight);
-        // prikazRecepta.setShowGrid(false);
-        //prikazRecepta.setShowHorizontalLines(true);
-        //dodajNamirnicuTabela.setGridColor(colorOldLace);
+        dodajNamirnicuTabela.setBounds(480, yLabelDefaultPosition, 520, dodajNamirnicuTabelaHeight);
         model.addColumn("Ime namirnice");
         model.addColumn("Kolicina");
         model.addColumn("Mjerna jedinica");
@@ -195,15 +188,33 @@ public class DodajReceptWindow extends JFrame {
         });
         dodajReceptiFrame.add(dodajNamirnicuTabela);
 
+        // Izbrisi namirnicu button
+        JButton izbrisiNamirnicuButton = new JButton("Izbriši sastojak");
+        izbrisiNamirnicuButton.setBounds(xTextFieldDefaultPosition, yLabelDefaultPosition + 50, 140, 30);
+        dodajReceptiFrame.add(izbrisiNamirnicuButton);
+        izbrisiNamirnicuButton.setVisible(false);
 
+        // Snimi recept
+        JButton snimiRecept = new JButton("Snimi recept");
+        snimiRecept.setBounds(xTextFieldDefaultPosition, yLabelDefaultPosition + 100, 140, 30);
+        dodajReceptiFrame.add(snimiRecept);
+        snimiRecept.setVisible(false);
+
+
+        int finalYLabelDefaultPosition = yLabelDefaultPosition;
         dodajSastojakButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dodajNamirnicuTabelaHeight = dodajNamirnicuTabelaHeight + 16;
-                dodajNamirnicuTabela.setBounds(50, 245, 500, dodajNamirnicuTabelaHeight);
-                model.addRow(new Object[]{"", "", "", ""});
-                dodajNamirnicuTabela.getColumnModel().getColumn(3).setCellEditor(new DefaultCellEditor(vrsteNamirnicaList));
-                dodajNamirnicuTabela.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(mjerneJediniceList));
+                int maxBrojNamirnica = 21;
+                if (model.getRowCount() <= maxBrojNamirnica) {
+                    dodajNamirnicuTabelaHeight = dodajNamirnicuTabelaHeight + 16;
+                    dodajNamirnicuTabela.setBounds(480, finalYLabelDefaultPosition, 520, dodajNamirnicuTabelaHeight);
+                    model.addRow(new Object[]{"", "", "", ""});
+                    dodajNamirnicuTabela.getColumnModel().getColumn(3).setCellEditor(new DefaultCellEditor(vrsteNamirnicaList));
+                    dodajNamirnicuTabela.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(mjerneJediniceList));
+                    izbrisiNamirnicuButton.setVisible(true);
+                    snimiRecept.setVisible(true);
+                }
             }
         });
 
@@ -213,10 +224,29 @@ public class DodajReceptWindow extends JFrame {
             public void actionPerformed(ActionEvent e) {
 //                String selectedBook = (String) kuhinjeList.getSelectedItem();
 //                System.out.println("You seleted the book: " + selectedBook);
-
-
             }
         });
+
+        izbrisiNamirnicuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = dodajNamirnicuTabela.getSelectedRow();
+                if(selectedRow!= 0) {
+                    try {
+                        model.removeRow(selectedRow);
+                        dodajNamirnicuTabelaHeight = dodajNamirnicuTabelaHeight - 16;
+                        dodajNamirnicuTabela.setBounds(480, finalYLabelDefaultPosition, 520, dodajNamirnicuTabelaHeight);
+                    } catch (java.lang.ArrayIndexOutOfBoundsException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                if (model.getRowCount() <= 1) {
+                    izbrisiNamirnicuButton.setVisible(false);
+                    snimiRecept.setVisible(false);
+                }
+            }
+        });
+
         dodajReceptiFrame.setVisible(true);
     }
 
